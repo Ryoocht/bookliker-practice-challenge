e045ea29557bd6449bddafbe4600d89eeb4f1b84
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-25 21:40:05 +1000

    Done with assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const baseURL = "http://localhost:3000/books";
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchBookList();
+});
+
+function fetchBookList(){
+    fetch(baseURL)
+    .then(resp => resp.json())
+    .then(result => {
+        result.forEach(bookObj => {
+            renderBookTitles(bookObj)
+        });
+    })
+}
+
+function renderBookTitles(bookObj){
+    let bookList = document.querySelector("#list");
+    let list = document.createElement("li");
+    list.innerText = bookObj.title;
+    list.id = bookObj.id;
+    bookList.appendChild(list);
+
+    list.addEventListener("click", e => {
+        const bookId = e.target.id;
+        removeLastChildDiv();
+        renderBookDetails(bookId);
+    });
+}
+
+function renderBookDetails(bookId){
+    fetch(`${baseURL}/${bookId}`)
+    .then(resp => resp.json())
+    .then(bookData => handleBookDetails(bookData))
+    .catch(err => console.log(err));
+}
+
+function handleBookDetails(details){
+    const detailDiv = document.createElement("div"),
+        bookCover = document.createElement("img"),
+        bookTitle = document.createElement("h3"),
+        bookSubtitle = document.createElement("h3"),
+        bookAuthor = document.createElement("h3"),
+        bookDesc = document.createElement("p"),
+        bookUserList = document.createElement("ul"),
+        bookUsers = document.createElement("li");
+    bookUserList.appendChild(bookUsers);
+    detailDiv.append(bookCover, bookTitle, bookSubtitle, bookAuthor, bookDesc, bookUserList);
+    document.querySelector("body").appendChild(detailDiv);
+
+    bookCover.src = details.img_url;
+    bookTitle.innerText = details.title;
+    bookSubtitle.innerText = details.subtitle;
+    bookAuthor.innerText = details.author;
+    bookDesc.innerText = details.description;
+    details.forEach(key => bookUsers.innerText = key.username);
+
+}
+
+function removeLastChildDiv(){
+    document.querySelector("body").lastChild.remove();
+}
